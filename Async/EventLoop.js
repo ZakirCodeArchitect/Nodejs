75a8aa326c8c6f817ab6d4e33dfa151dc1951206
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-10-05 23:17:00 +0500

    Event Loop

---
+++ Async/EventLoop.js
@@ -0,0 +1,10 @@
+Promise.resolve().then(() => console.log(1));
+
+setTimeout(() => console.log(2), 10);
+
+queueMicrotask(() => {
+    console.log(3);
+    queueMicrotask(() => console.log(4))
+});
+
+console.log(5);
