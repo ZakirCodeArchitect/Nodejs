064fae47a80adb322095b35e50b95e519ec8a8cc
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-10-23 00:11:29 +0500

    web server

--- webServer/index.js
+++ webServer/index.js
@@ -6,26 +6,54 @@ const filePath = path.join(__dirname, "data.json")
 const index = path.join(__dirname, "index.html")
 
 const page = fs.readFileSync(index, "utf-8")
-const data = fs.readFileSync(filePath, 'utf-8')
+const data = JSON.parse(fs.readFileSync(filePath, 'utf-8')); // parsing into json 
+// because server only deals in strings 
+
+const products = data.products // from the data.json file 
 
 const server = http.createServer((request, response) => {
     console.log("server is running in port 3000")
 
+    // this is how we are displaying the details of the product entered on the browser with ID
+    if (request.url.startsWith('/product')) {
+        const userId = request.url.split('/')[2]; // id that is being entered on the browser
+        const product = products.find(p => p.userId === (+userId));
+        // console.log(product)
+
+        response.setHeader('Content-Type', 'text/html');
+
+        // now with this replace method i had fetched the data from the .json file and displayed it in the title of the html page
+        let modifiedDetails = page.replace("**title**", product.title)
+            .replace("**body**", product.body)
+            .replace("**price**", product.price)
+        response.end(modifiedDetails);
+        return; // this is very important because other wise server will go onward to search more and you'll get an error
+    }
+
+
+    // case '/':
+    //     response.setHeader('Content-Type', 'text/html');
+
+    //     // now with this replace method i had fetched the data from the .json file and displayed it in the title of the html page
+    //     let modifiedDetails = page.replace("**title**", product.title)
+    //         .replace("**body**", product.body)
+    //         .replace("**price**", product.price)
+    //     response.end(modifiedDetails)
+    //     break;
+
     // routing created
     switch (request.url) {
         case '/':
-            response.setHeader('Content-Type', 'text/html');
-            // response.setHeader('Content-Type', 'application/json');
-            response.end(page)
+            response.end("Home Page");
             break;
 
         case '/dashboard':
             response.setHeader('Content-Type', 'application/json');
-            response.end(data)
+            response.end(JSON.stringify(data));
             break;
 
         default:
-            response.end(404, "Status not found")
+            response.end("Status not found")
             break;
     }
 })
