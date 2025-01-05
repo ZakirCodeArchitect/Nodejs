26b2bc88b4515e73029dbcc8db3e6d1aec951f55
Zair Matloob <zakirmatloob149@gmail.com>
2025-01-05 23:25:13 +0500

    revisions

--- ErrorHandling/asyncAwait.js
+++ ErrorHandling/asyncAwait.js
@@ -16,4 +16,18 @@ const someFunction = async () => {
     } 
 }
 
-someFunction()
+someFunction();
+
+// fetch('https://api.example.com/data')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('There has been a problem with your fetch operation:', error);
+//   });
